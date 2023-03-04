cf4fddf5e3ccae7c0016a3f3e7893a23bafc681b
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-04 02:40:07 +0000

    added project structure

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# my-react-portfolio
+
+## Credits
+Folder Structure: https://www.xenonstack.com/insights/reactjs-project-structure
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//import { Grid, Cell,Menu,MenuItem } from "react-foundation";
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
