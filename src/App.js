59196d6ae9cb040309cc7c2a192da73c09bab651
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-08 23:55:21 +0000

    removed NotFound page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 //import { useState } from "react";
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 import Work from "./pages/Work";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
+//import NotFound from "./pages/NotFound";
 
 
 
@@ -25,11 +25,10 @@ function App() {
        
         <Header />
         <Routes>         
-          <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
+          
         </Routes>
         <Footer />
       </div>
